5a1e9ad1cb693feb73d97fe9b37651d9e5c64996
Timo Loewe <timoloewe91@googlemail.com>
2012-10-28 23:57:41 +0100

    back to coverflow switcher

--- CoverflowAltTab@dmo60.de/manager.js
+++ CoverflowAltTab@dmo60.de/manager.js
@@ -12,7 +12,6 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const CoverflowAltTab = ExtensionUtils.getCurrentExtension();
 
 const Switcher = CoverflowAltTab.imports.switcher;
-const Win7Switcher = CoverflowAltTab.imports.win7switcher;
 
 /**
  * This class handles window events, so we can keep a stack of windows ordered
@@ -92,7 +91,7 @@ Manager.prototype = {
 					currentIndex = -1;
 				}
 				
-				let switcher = new Win7Switcher.Switcher(windows, actions, mask, currentIndex);
+				let switcher = new Switcher.Switcher(windows, actions, mask, currentIndex);
 			};
 		},
 };
