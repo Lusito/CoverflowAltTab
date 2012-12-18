5ed99d7933b72db2ea00fc03087bf3fe0387aa74
Timo Loewe <timoloewe91@gmail.com>
2012-12-18 21:43:00 +0100

    make panel and hot corner insensitive during Coverflow + tried to make identation/whitespaces consistent

--- CoverflowAltTab@dmo60.de/switcher.js
+++ CoverflowAltTab@dmo60.de/switcher.js
@@ -33,9 +33,9 @@ const INITIAL_DELAY_TIMEOUT = 150;
 
 
 ///*
-// * SET ICON SIZE AND SPACING BETWEEN ICON AND WINDOW TITLE HERE:
-// * --------------------------------------------------------
-// */
+//* SET ICON SIZE AND SPACING BETWEEN ICON AND WINDOW TITLE HERE:
+//* --------------------------------------------------------
+//*/
 const ICON_SIZE = 64;  // default: 64
 const ICON_SIZE_BIG = 128;  // default: 128
 const ICON_TITLE_SPACING = 10;  // default: 10
@@ -58,20 +58,20 @@ Switcher.prototype = {
 			this._actions = actions;
 			this._haveModal = false;
 			this._tracker = Shell.WindowTracker.get_default();
-			this._shellwm =  global.window_manager;
-			
+			this._shellwm = global.window_manager;
+
 			this._dcid = this._shellwm.connect('destroy', Lang.bind(this, this._windowDestroyed));
 			this._mcid = this._shellwm.connect('map', Lang.bind(this, this._activateSelected));
-			
+
 			this._background = Meta.BackgroundActor.new_for_screen(global.screen);
-	        this._background.hide();
-	        global.overlay_group.add_actor(this._background);
-			
-	        // create a container for all our widgets
+			this._background.hide();
+			global.overlayGroup.add_actor(this._background);
+
+			// create a container for all our widgets
 			this.actor = new St.Widget({ visible: true, reactive: true, });
 			this.actor.hide();
-			global.overlay_group.add_actor(this.actor);
-			
+			Main.uiGroup.add_actor(this.actor);
+
 			if (!Main.pushModal(this.actor)) {
 				return false;
 			}
@@ -82,7 +82,7 @@ Switcher.prototype = {
 			this.actor.connect('key-press-event', Lang.bind(this, this._keyPressEvent));
 			this.actor.connect('key-release-event', Lang.bind(this, this._keyReleaseEvent));
 			this.actor.connect('scroll-event', Lang.bind(this, this._scrollEvent));
-			
+
 			// There's a race condition; if the user released Alt before
 			// we got the grab, then we won't be notified. (See
 			// https://bugzilla.gnome.org/show_bug.cgi?id=596695 for
@@ -92,14 +92,14 @@ Switcher.prototype = {
 			if (!(mods & this._modifierMask)) {
 				this._activateSelected();
 			}
-			
+
 			this._initialDelayTimeoutId = Mainloop.timeout_add(INITIAL_DELAY_TIMEOUT,
-                    Lang.bind(this, this.show));
+					Lang.bind(this, this.show));
 		},
 
-		show: function() {			
+		show: function() {
 			let monitor = Main.layoutManager.primaryMonitor;
-			
+
 			// create previews
 			let currentWorkspace = global.screen.get_active_workspace();
 			this._previews = [];
@@ -124,41 +124,41 @@ Switcher.prototype = {
 						x: (metaWin.minimized) ? 0 : compositor.x + compositor.width / 2,
 						y: (metaWin.minimized) ? 0 : compositor.y + compositor.height / 2
 					});
-										
+
 					clone.target_width = Math.round(width * scale);
 					clone.target_height = Math.round(height * scale);
 					clone.target_width_side = clone.target_width * 2/3;
 					clone.target_height_side = clone.target_height;
-										
+
 					this._previews.push(clone);
 					this.actor.add_actor(clone);
 				};
 			}
-			
+
 			// hide windows and show Coverflow actors
 			global.window_group.hide();
 			this.actor.show();
 			this._background.show();
-	        
+
 			if (hide_panel) {
-				Tweener.addTween(Main.panel.actor, 
-					{ opacity: 0,
-					  time: animation_time,
-					  transistion: 'easeOutQuad'}
-				);
+				Tweener.addTween(Main.panel.actor, {
+					opacity: 0,
+					time: animation_time,
+					transistion: 'easeOutQuad'
+				});
 			}
-			
-			Tweener.addTween(this._background,
-                    { dim_factor: dim_factor,
-                      time: animation_time,
-                      transition: 'easeOutQuad'
-                    });
-			
+
+			Tweener.addTween(this._background, {
+				dim_factor: dim_factor,
+				time: animation_time,
+				transition: 'easeOutQuad'
+			});
+
 			this._initialDelayTimeoutId = 0;
-			
+
 			this._next();
 		},
-		
+
 		// If next() is called on the last window, we want to
 		// trigger a loop animation: calling previous() until we
 		// are back on the first window and accelerate animations.
@@ -176,7 +176,7 @@ Switcher.prototype = {
 			}
 			this.actor.set_reactive(true);
 		},
-		
+
 		// The same here like in next(),
 		// but of course the other way around
 		_previous: function(loop) {
@@ -198,9 +198,9 @@ Switcher.prototype = {
 			// (currently time = 0, so there is no animation at all) 
 			let flow_time = loop ? 0.0 : animation_time;
 			let transition_type = loop ? 'linear' : 'easeOutQuad';
-			
+
 			let monitor = Main.layoutManager.primaryMonitor;
-			
+
 			let app_icon_size;
 			let label_offset;
 			if (icon_style == "Classic") {
@@ -210,7 +210,7 @@ Switcher.prototype = {
 				app_icon_size = ICON_SIZE_BIG;
 				label_offset = 0;
 			}
-			
+
 			// window title label
 			if (this._windowTitle) {
 				Tweener.addTween(this._windowTitle, {
@@ -270,7 +270,7 @@ Switcher.prototype = {
 					anchor_gravity: Clutter.Gravity.CENTER,
 					x: Math.round(this._windowTitle.x - (this._windowTitle.width + app_icon_size) / 2 - ICON_TITLE_SPACING),
 					y: this._windowTitle.y
-			    });
+				});
 			} else {
 				this._applicationIconBox = new St.Bin({
 					style_class: 'coverflow-app-icon-box',
@@ -278,8 +278,8 @@ Switcher.prototype = {
 					height: app_icon_size * 1.15,
 					opacity: 0,
 					x: (monitor.width - app_icon_size) / 2,
-				    y: (monitor.height - app_icon_size) / 2,
-			    });
+					y: (monitor.height - app_icon_size) / 2,
+				});
 			}
 			this._applicationIconBox.add_actor(this._icon);
 			this.actor.add_actor(this._applicationIconBox);
@@ -288,7 +288,6 @@ Switcher.prototype = {
 				time: flow_time,
 				transition: transition_type,
 			});
-			
 
 			// preview windows
 			for (i in this._previews) {
@@ -296,7 +295,7 @@ Switcher.prototype = {
 
 				if (i == this._currentIndex) {
 					let rotation_vertex_x = (preview.get_anchor_point_gravity() == Clutter.Gravity.EAST) ?
-											preview.width / 2 : -preview.width / 2;
+							preview.width / 2 : -preview.width / 2;
 					preview.move_anchor_point_from_gravity(Clutter.Gravity.CENTER);
 					preview.rotation_center_y = new Clutter.Vertex({ x: rotation_vertex_x, y: 0.0, z: 0.0 });
 					preview.raise_top();
@@ -311,7 +310,7 @@ Switcher.prototype = {
 						time: flow_time,
 						transition: transition_type
 					});
-					
+
 				} else if (i < this._currentIndex) {
 					preview.move_anchor_point_from_gravity(Clutter.Gravity.WEST);
 					preview.rotation_center_y = new Clutter.Vertex({ x: 0.0, y: 0.0, z: 0.0 });
@@ -326,7 +325,7 @@ Switcher.prototype = {
 						time: flow_time,
 						transition: transition_type
 					});
-					
+
 				} else if (i > this._currentIndex) {
 					preview.move_anchor_point_from_gravity(Clutter.Gravity.EAST);
 					preview.rotation_center_y = new Clutter.Vertex({ x: 0.0, y: 0.0, z: 0.0 });
@@ -348,7 +347,6 @@ Switcher.prototype = {
 			};
 		},
 
-		
 		// Called by every window on the right side on animation completion,
 		// because if we do a loop, we want to know when a next() or previous()
 		// shift is finished
@@ -357,14 +355,14 @@ Switcher.prototype = {
 			// do nothing
 			if (!loop || index != this._windows.length-1)
 				return;
-			
+
 			// otherwise do the loop by calling next()/previous() again
 			if (direction == "next")
 				this._next(true);
 			else
 				this._previous(true);
 		},
-		
+
 		_keyPressEvent: function(actor, event) {
 			let keysym = event.get_key_symbol();
 			let event_state = event.get_state();
@@ -374,18 +372,18 @@ Switcher.prototype = {
 			// Esc -> close CoverFlow
 			if (keysym == Clutter.Escape) {
 				this.destroy();
-			// Q -> Close window
-		    } else if (keysym == Clutter.q || keysym == Clutter.Q) {
+				// Q -> Close window
+			} else if (keysym == Clutter.q || keysym == Clutter.Q) {
 				this._actions['remove_selected'](this._windows[this._currentIndex]);
-			// Left/Right -> navigate through previews	
+				// Left/Right -> navigate through previews	
 			} else if (keysym == Clutter.Right) {
 				this._next();
 			} else if (keysym == Clutter.Left) {
 				this._previous();
-			// D -> Show desktop
+				// D -> Show desktop
 			} else if (keysym == Clutter.d || keysym == Clutter.D) {
 				this._showDesktop();
-			// default alt-tab
+				// default alt-tab
 			} else if (action == Meta.KeyBindingAction.SWITCH_GROUP ||
 					action == Meta.KeyBindingAction.SWITCH_WINDOWS ||
 					action == Meta.KeyBindingAction.SWITCH_PANELS) {
@@ -407,7 +405,7 @@ Switcher.prototype = {
 
 			return true;
 		},
-		
+
 		// allow navigating by mouse-wheel scrolling
 		_scrollEvent: function(actor, event) {
 			actor.set_reactive(false);
@@ -416,10 +414,10 @@ Switcher.prototype = {
 			actor.set_reactive(true);
 			return true;
 		},
-		
+
 		_windowDestroyed: function(shellwm, actor) {
 			let window = actor.meta_window;
-			
+
 			for (i in this._windows) {
 				if (window == this._windows[i]) {
 					if (this._windows.length == 1) {
@@ -436,12 +434,12 @@ Switcher.prototype = {
 				}
 			}
 		},
-		
+
 		_activateSelected: function() {
 			this._actions['activate_selected'](this._windows[this._currentIndex]);
 			this.destroy();
 		},
-		
+
 		_showDesktop: function() {
 			for (i in this._windows) {
 				if (!this._windows[i].minimized)
@@ -451,8 +449,8 @@ Switcher.prototype = {
 		},
 
 		_onHideBackgroundCompleted: function() {
-			global.overlay_group.remove_actor(this.actor);
-			global.overlay_group.remove_actor(this._background);
+			global.overlayGroup.remove_actor(this._background);
+			Main.uiGroup.remove_actor(this.actor);
 
 			// show all window actors
 			global.window_group.show();
@@ -460,7 +458,7 @@ Switcher.prototype = {
 
 		_onDestroy: function() {
 			let monitor = Main.layoutManager.primaryMonitor;
-			
+
 			if (this._initialDelayTimeoutId == 0) {
 				// preview windows
 				let currentWorkspace = global.screen.get_active_workspace();
@@ -468,7 +466,7 @@ Switcher.prototype = {
 					let preview = this._previews[i];
 					let metaWin = this._windows[i];
 					let compositor = this._windows[i].get_compositor_private();
-					
+
 					if (i != this._currentIndex)
 						preview.lower_bottom();
 					let rotation_vertex_x = 0.0;
@@ -479,10 +477,10 @@ Switcher.prototype = {
 					}
 					preview.move_anchor_point_from_gravity(Clutter.Gravity.CENTER);
 					preview.rotation_center_y = new Clutter.Vertex({ x: rotation_vertex_x, y: 0.0, z: 0.0 });
-					
+
 					Tweener.addTween(preview, {
 						opacity: (!metaWin.minimized && metaWin.get_workspace() == currentWorkspace 
-								  || metaWin.is_on_all_workspaces()) ? 255 : 0,
+								|| metaWin.is_on_all_workspaces()) ? 255 : 0,
 						x: (metaWin.minimized) ? 0 : compositor.x + compositor.width / 2,
 						y: (metaWin.minimized) ? 0 : compositor.y + compositor.height / 2,
 						width: (metaWin.minimized) ? 0 : compositor.width,
@@ -492,21 +490,21 @@ Switcher.prototype = {
 						transition: 'easeOutQuad',
 					});
 				}
-				
+
 				// window title and icon
 				this._windowTitle.hide();
 				this._applicationIconBox.hide();
-				
+
 				// panel
 				if (hide_panel) {
 					Tweener.removeTweens(Main.panel.actor);
-					Tweener.addTween(Main.panel.actor, 
-						{ opacity: 255,
-						  time: animation_time,
-						  transistion: 'easeOutQuad'}
+					Tweener.addTween(Main.panel.actor, {
+						opacity: 255,
+						time: animation_time,
+						transistion: 'easeOutQuad'}
 					);
 				}
-				
+
 				// background
 				Tweener.removeTweens(this._background);
 				Tweener.addTween(this._background, {
@@ -514,26 +512,26 @@ Switcher.prototype = {
 					time: animation_time,
 					transition: 'easeOutQuad',
 					onComplete: Lang.bind(this, this._onHideBackgroundCompleted),
-				});
-				
-			}
+					});
 
-			if (this._haveModal) {
-				Main.popModal(this.actor);
-				this._haveModal = false;
-			}
-			
-			if (this._initialDelayTimeoutId != 0)
-				Mainloop.source_remove(this._initialDelayTimeoutId);
-			
-			this._shellwm.disconnect(this._dcid);
-			this._shellwm.disconnect(this._mcid);
-			this._windows = null;
-			this._windowTitle = null;
-			this._icon = null;
-			this._applicationIconBox = null;
-			this._previews = null;
-			this._initialDelayTimeoutId = null;
+				}
+
+				if (this._haveModal) {
+					Main.popModal(this.actor);
+					this._haveModal = false;
+				}
+
+				if (this._initialDelayTimeoutId != 0)
+					Mainloop.source_remove(this._initialDelayTimeoutId);
+
+				this._shellwm.disconnect(this._dcid);
+				this._shellwm.disconnect(this._mcid);
+				this._windows = null;
+				this._windowTitle = null;
+				this._icon = null;
+				this._applicationIconBox = null;
+				this._previews = null;
+				this._initialDelayTimeoutId = null;
 		},
 
 		destroy: function() {
